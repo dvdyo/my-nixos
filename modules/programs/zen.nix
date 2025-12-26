c87bef8c1f0d31aedf44883365b3b20f5e2b22e5
dvd <deevidee@proton.me>
2025-12-26 05:48:34 +0200

    added policy overrides to zen browser

--- modules/programs/zen.nix
+++ modules/programs/zen.nix
@@ -13,7 +13,26 @@ in
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [
-      inputs.zen-browser.packages."${pkgs.system}".default
+      (inputs.zen-browser.packages."${pkgs.system}".default.override {
+        policies = {
+          AutofillAddressEnabled = true;
+          AutofillCreditCardEnabled = false;
+          DisableAppUpdate = true;
+          DisableFeedbackCommands = true;
+          DisableFirefoxStudies = true;
+          DisablePocket = true;
+          DisableTelemetry = true;
+          DontCheckDefaultBrowser = true;
+          NoDefaultBookmarks = true;
+          OfferToSaveLogins = false;
+          EnableTrackingProtection = {
+            Value = true;
+            Locked = true;
+            Cryptomining = true;
+            Fingerprinting = true;
+          };
+        };
+      })
     ];
   };
 }
