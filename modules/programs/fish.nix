0698a2b37e3340e05f63c15161484c779bb58dd7
dvd <deevidee@proton.me>
2025-12-26 17:45:05 +0200

    borked up fish config, attempting to fix

--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -57,7 +57,7 @@ in
       config = ''
         # Custom Functions
         function mark_prompt_start --on-event fish_prompt
-            echo -en "\e]133;A\e\"
+            echo -en "\\e]133;A\\e\\\\"
         end
 
         # Colors
