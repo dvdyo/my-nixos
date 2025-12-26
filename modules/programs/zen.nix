eeb42f39ae82b1c3dd7196543827ab908631442b
dvd <deevidee@proton.me>
2025-12-26 06:05:08 +0200

    fixed policy overrides 2.0?

--- modules/programs/zen.nix
+++ modules/programs/zen.nix
@@ -7,32 +7,42 @@
 }:
 let
   cfg = config.custom.programs.zen;
+  policies = {
+    AutofillAddressEnabled = true;
+    AutofillCreditCardEnabled = false;
+    DisableAppUpdate = true;
+    DisableFeedbackCommands = true;
+    DisableFirefoxStudies = true;
+    DisablePocket = true;
+    DisableTelemetry = true;
+    DontCheckDefaultBrowser = true;
+    NoDefaultBookmarks = true;
+    OfferToSaveLogins = false;
+    EnableTrackingProtection = {
+      Value = true;
+      Locked = true;
+      Cryptomining = true;
+      Fingerprinting = true;
+    };
+  };
 in
 {
   options.custom.programs.zen.enable = lib.mkEnableOption "Enable Zen Browser";
 
   config = lib.mkIf cfg.enable {
+    # 1. Install the vanilla browser package
     environment.systemPackages = [
-      (inputs.zen-browser.packages."${pkgs.system}".default.override {
-        extraPolicies = {
-          AutofillAddressEnabled = true;
-          AutofillCreditCardEnabled = false;
-          DisableAppUpdate = true;
-          DisableFeedbackCommands = true;
-          DisableFirefoxStudies = true;
-          DisablePocket = true;
-          DisableTelemetry = true;
-          DontCheckDefaultBrowser = true;
-          NoDefaultBookmarks = true;
-          OfferToSaveLogins = false;
-          EnableTrackingProtection = {
-            Value = true;
-            Locked = true;
-            Cryptomining = true;
-            Fingerprinting = true;
-          };
-        };
-      })
+      inputs.zen-browser.packages."${pkgs.system}".default
     ];
+
+    # 2. Deploy policies to /etc/firefox (standard location)
+    environment.etc."firefox/policies/policies.json".text = builtins.toJSON {
+      inherit policies;
+    };
+
+    # 3. Deploy policies to /etc/zen (Zen-specific location, just in case)
+    environment.etc."zen/policies/policies.json".text = builtins.toJSON {
+      inherit policies;
+    };
   };
 }
