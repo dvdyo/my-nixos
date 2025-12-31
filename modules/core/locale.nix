7f0b1c60f9e31485664f4482110ca0912ec9ce97
dvd <deevidee@proton.me>
2025-12-31 13:19:22 +0200

    uh... refactor came with some errors

--- modules/core/locale.nix
+++ modules/core/locale.nix
@@ -1,5 +1,17 @@
-{ ... }:
 {
-  time.timeZone = "UTC"; # Host file should override this
-  i18n.defaultLocale = "en_US.UTF-8";
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.custom.core.locale;
+  inherit (lib) mkEnableOption mkIf;
+in
+{
+  options.custom.core.locale.enable = mkEnableOption "Enable Standard Locale/Timezone Settings";
+
+  config = mkIf cfg.enable {
+    time.timeZone = lib.mkDefault "UTC"; # Host file can override this
+    i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
+  };
 }
--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -1,27 +1,36 @@
 {
   lib,
+  config,
   inputs,
   ...
 }:
+let
+  cfg = config.custom.core.nix;
+  inherit (lib) mkEnableOption mkIf mkDefault;
+in
 {
-  nixpkgs.config.allowUnfree = lib.mkDefault true;
+  options.custom.core.nix.enable = mkEnableOption "Enable Standard Nix Settings";
 
-  nix =
-    let
-      flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-    in
-    {
-      settings = {
-        experimental-features = "nix-command flakes";
-        trusted-users = [ "@wheel" ];
-        # Optimization: make flake commands faster by using local pins
-        flake-registry = "";
+  config = mkIf cfg.enable {
+    nixpkgs.config.allowUnfree = mkDefault true;
+
+    nix =
+      let
+        flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+      in
+      {
+        settings = {
+          experimental-features = "nix-command flakes";
+          trusted-users = [ "@wheel" ];
+          # Optimization: make flake commands faster by using local pins
+          flake-registry = "";
+        };
+
+        # Match registry and nix-path to our flake inputs
+        registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
+        nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+
+        optimise.automatic = true;
       };
-      
-      # Match registry and nix-path to our flake inputs
-      registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
-      nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-
-      optimise.automatic = true;
-    };
+  };
 }
