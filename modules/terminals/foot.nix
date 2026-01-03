de2b0cc17071053c8fda2fec9d3e3acb138ba1ab
dvd <dvd@example.com>
2026-01-03 06:08:29 +0000

    added some fixes for foot because i am stupid

--- modules/terminals/foot.nix
+++ modules/terminals/foot.nix
@@ -12,9 +12,13 @@ in
   options.custom.terminals.foot.enable = mkEnableOption "Enable Foot Terminal";
 
   config = mkIf cfg.enable {
-    # 1. Enable Hjem Rum Foot module
+    # 1. Enable System-level Foot (Provides Shell Integration automatically)
+    programs.foot.enable = true;
+
+    # 2. Enable Hjem Rum Foot module (Provides User Config)
     custom.hjem.cfg.rum.programs.foot = {
       enable = true;
+      package = null; # Use system package
       settings = {
         main = {
           font = "JetBrainsMono Nerd Font:size=11";
@@ -33,7 +37,15 @@ in
       };
     };
 
-    # 2. Enable foot-server systemd service for instant startup via footclient
-    services.foot-server.enable = true;
+    # 3. Manually define foot-server service (Since Hjem/NixOS don't provide it)
+    systemd.user.services.foot-server = {
+      description = "Foot Terminal Server";
+      wantedBy = [ "graphical-session.target" ];
+      partOf = [ "graphical-session.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.foot}/bin/foot --server";
+        Restart = "always";
+      };
+    };
   };
 }
