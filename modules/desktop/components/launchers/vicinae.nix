73f8565c5c602e9c548344a9c658c7f9758af732
dvd <dvd@example.com>
2026-01-04 20:47:24 +0000

    attempt to decouple vicinae and niri v1

--- modules/desktop/components/launchers/vicinae.nix
+++ modules/desktop/components/launchers/vicinae.nix
@@ -13,5 +13,25 @@ in
 
   config = mkIf cfg.enable {
     environment.systemPackages = [ pkgs.vicinae ];
+    # Register implementation
+    # custom.interface.launcher.command = "vicinae toggle";
+
+    # Systemd User Service for vicinae-server
+    systemd.user.services.vicinae-server = {
+      description = "Vicinae Launcher Daemon";
+      wantedBy = [ "graphical-session.target" ];
+      partOf = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+
+      # Force PATH to ensure launched apps can find their binaries
+      environment.PATH = lib.mkForce "/run/wrappers/bin:/etc/profiles/per-user/%u/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin";
+
+      serviceConfig = {
+        # Using the absolute path to the binary
+        ExecStart = "${pkgs.vicinae}/bin/vicinae server --replace";
+        Restart = "always";
+        RestartSec = 5;
+      };
+    };
   };
 }
