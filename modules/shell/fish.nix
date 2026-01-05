d4372a03eee92e0700e7fec722011f63f23f478c
dvd <dvd@example.com>
2026-01-05 02:56:04 +0000

    i am tiredd

--- modules/shell/fish.nix
+++ modules/shell/fish.nix
@@ -43,7 +43,7 @@ in
         la = "ls -a";
         lla = "ls -la";
         ez = "eza --icons --group-directories-first -1";
-	zd = "zoxide"
+      	zd = "zoxide";
       };
 
       # 2. Aliases (don't work?)
