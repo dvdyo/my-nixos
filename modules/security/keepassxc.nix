75db5a827ba9dd1f826d81eb1945b4897c080ba0
dvd <dvd@example.com>
2026-01-06 01:10:58 +0000

    changed keepassxc to be a hjem-rum module

--- modules/security/keepassxc.nix
+++ modules/security/keepassxc.nix
@@ -14,8 +14,24 @@ in
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = [ 
-      pkgs.keepassxc
-    ];
+    # We use the Hjem Rum module to manage the package and its configuration
+    custom.hjem.cfg.rum.programs.keepassxc = {
+      enable = true;
+      package = pkgs.keepassxc;
+      settings = {
+        General = {
+          ConfigVersion = 2;
+        };
+        GUI = {
+          # No System Tray in Niri (by default), so do NOT minimize to tray.
+          MinimizeToTray = false;
+          MinimizeOnClose = false;
+          ShowTrayIcon = false;
+        };
+        Browser = {
+          Enabled = true;
+        };
+      };
+    };
   };
 }
