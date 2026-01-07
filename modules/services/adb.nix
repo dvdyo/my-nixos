e053f7b13da62a5f41375acef319c05c6bc29c2b
dvd <dvd@example.com>
2026-01-07 18:55:14 +0000

    failed assertion during build (related to some adb fuckery)

--- modules/services/adb.nix
+++ modules/services/adb.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 let
@@ -12,10 +13,11 @@ in
     mkEnableOption "Enable Android System Prerequisites (KVM + ADB)";
 
   config = mkIf cfg.enable {
-    # Prerequisite 1: ADB Daemon and Udev rules
-    programs.adb.enable = true;
+    # Systemd 258+ handles uaccess rules automatically. 
+    # We just need the tools and the group for some legacy software.
+    environment.systemPackages = [ pkgs.android-tools ];
 
-    # Prerequisite 2: User groups for ADB
+    # User groups for ADB
     users.users.${config.custom.core.user.name}.extraGroups = [
       "adbusers"
     ];
