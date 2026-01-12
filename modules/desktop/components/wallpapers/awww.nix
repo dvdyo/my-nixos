d4b29668a33fd754934683a2fb37bae0ba0faa3c
dvd <dvd@example.com>
2026-01-12 12:43:00 +0000

    some refactoring 3

--- modules/desktop/components/wallpapers/awww.nix
+++ modules/desktop/components/wallpapers/awww.nix
@@ -35,7 +35,7 @@ in
         ExecStart = awww-daemon;
         
         # Wait until the daemon detects wayland outputs before restoring
-        ExecStartPost = ''${sh} -c 'while ! ${awww} query | ${grep} -q ":"; do ${sleep} 0.2; done; ${awww} restore''';
+        ExecStartPost = "${sh} -c 'while ! ${awww} query | ${grep} -q \":\": do ${sleep} 0.2; done; ${awww} restore'";
 
         Restart = "always";
         RestartSec = "1s";
