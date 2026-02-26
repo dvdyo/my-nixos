da8c6b5c8ef024bda0c26731b8c662f0f5b0b6b9
dvd <dvd@example.com>
2026-02-26 17:38:30 +0200

    trying out nixvirt 4

--- modules/virtualization/domains.nix
+++ modules/virtualization/domains.nix
@@ -63,7 +63,7 @@ in
             vcpu = { count = 4; };
             storage_vol = "/var/lib/libvirt/images/win11.qcow2";
             install_vol = "/var/lib/libvirt/iso/win11.iso";
-            nvram_path = "/var/lib/libvirt/nvram/win11.nvram";
+            nvram_path = "/var/lib/libvirt/qemu/nvram/win11.nvram";
             virtio_net = true;
             virtio_drive = true;
             install_virtio = true;
