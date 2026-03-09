7da5a622a3f639168550c9d02f029162ed129e52
dvd <dvd@example.com>
2026-03-09 17:05:01 +0200

    added firefox 5

--- modules/browsers/firefox/default.nix
+++ modules/browsers/firefox/default.nix
@@ -1,18 +1,22 @@
-{ self, lib, config, ... }:
-let
-  cfg = config.custom.browsers.firefox;
-  inherit (lib) mkEnableOption mkIf;
-in
+_:
 {
-  options.custom.browsers.firefox.enable = mkEnableOption "Enable firefox";
-  config = mkIf cfg.enable {
-    features = {
-      browser = "firefox";
-      pdfs = "firefox";
-    };
-
-    environment.systemPackages = [ self.wrappers.firefox.drv ];
-
-    environment.variables.BROWSER = "firefox"; # `man` likes having this
+  options = {
+    policiesFiles.default = [
+      ./policies/extensions.json
+      ./policies/policies.json
+      ./policies/preferences.json
+      ./policies/searchEngines.json
+    ];
+    autoConfigFiles.defaultFunc =
+      { inputs }:
+      let
+        inherit (inputs.nixpkgs) pkgs;
+        inherit (pkgs) replaceVars;
+      in [
+        (replaceVars ./autoConfig.js {
+          userChromeFile = ./userChrome.css;
+          bookmarksFile = ./bookmarks.html;
+        })
+      ];
   };
 }
