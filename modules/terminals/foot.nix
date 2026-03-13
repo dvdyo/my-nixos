f388a0019d06b3284a46570689c28c186cd987f1
dvd <dvd@example.com>
2026-03-13 09:47:10 +0200

    Revert "trying to get rid of the foot's warning" + "flake update"
    This reverts commit 2e9d23106db9d5d558938f869a5f099e28ecc58d.

--- modules/terminals/foot.nix
+++ modules/terminals/foot.nix
@@ -32,7 +32,7 @@ in
         mouse = {
           hide-when-typing = "yes";
         };
-        colors-dark = {
+        colors = {
           alpha = 0.95;
           
           # Gruvbox Material Dark (Medium)
