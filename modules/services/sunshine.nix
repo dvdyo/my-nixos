d84e5237249f721f8ddd01364b05633c9a327a8a
dvd <dvd@example.com>
2026-06-01 00:52:25 +0300

    added sunshine service

---
+++ modules/services/sunshine.nix
@@ -0,0 +1,25 @@
+{
+  lib,
+  config,
+  pkgs,
+  username,
+  ...
+}:
+let
+  cfg = config.custom.services.sunshine;
+  inherit (lib) mkEnableOption mkIf;
+in
+{
+  options.custom.services.sunshine.enable = mkEnableOption "Enable sunshine";
+
+  config = mkIf cfg.enable {
+  services.sunshine = {
+    enable = true;
+    autoStart = true;
+    capSysAdmin = true;
+    openFirewall = true;
+    };
+
+    users.users.${username}.extraGroups = [ "uinput" ];
+    };
+}
