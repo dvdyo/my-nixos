a78989c98feaff2c6b96cc290ed25c17bb57e14b
dvd <dvd@example.com>
2026-06-11 18:07:21 +0300

    firejjailed cisco

--- modules/studying/ciscoPacketTracer/default.nix
+++ modules/studying/ciscoPacketTracer/default.nix
@@ -9,9 +9,20 @@ let
   inherit (lib) mkEnableOption mkIf;
 in
 {
-  options.custom.studying.ciscoPacketTracer.enable = mkEnableOption "Enable ciscoPacketTracer ";
-
+  options.custom.studying.ciscoPacketTracer.enable = mkEnableOption "Enable ciscoPacketTracer";
   config = mkIf cfg.enable {
-    environment.systemPackages = [ pkgs.ciscoPacketTracer9 ];
+    programs.firejail = {
+      enable = true;
+      wrappedBinaries = {
+        packettracer9 = {
+          executable = lib.getExe pkgs.ciscoPacketTracer9;
+          desktop = "${pkgs.ciscoPacketTracer9}/share/applications/cisco-pt9.desktop";
+          extraArgs = [
+            "--net=none"
+            "--noprofile"
+          ];
+        };
+      };
+    };
   };
 }
