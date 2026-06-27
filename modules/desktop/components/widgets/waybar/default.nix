7a114373e4590b00f876768a5a316a40e52b898b
dvd <dvd@example.com>
2026-06-27 22:23:03 +0300

    Revert "sloppy 3"
    This reverts commit 32efc8c9399edebc0eb60bc371cf4d8c1f0937fe.

--- modules/desktop/components/widgets/waybar/default.nix
+++ modules/desktop/components/widgets/waybar/default.nix
@@ -1,19 +1,8 @@
 {
-  lib,
-  config,
-  pkgs,
   ...
 }:
-let
-  cfg = config.custom.desktop.components.widgets.waybar;
-  inherit (lib) mkEnableOption mkIf;
-in
 {
-  options.custom.desktop.components.widgets.waybar.enable = mkEnableOption "waybar";
-
-  config = mkIf cfg.enable {
-    programs.waybar.enable = true;
-    programs.waybar.target = true;
-    };
-  };
+  imports = [
+    ./waybar
+  ];
 }
