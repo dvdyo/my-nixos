bb470c3906c77214f8f2a452c094b019e2d01b64
dvd <dvd@example.com>
2026-06-27 15:39:14 +0300

    added qutebrowser 1231213212312312

--- modules/desktop/xdg.mime.nix
+++ modules/desktop/xdg.mime.nix
@@ -3,8 +3,8 @@
   xdg.mime.enable = true;
   xdg.mime.defaultApplications = {
     "application/pdf" = "org.pwmt.zathura.desktop";
-    "text/html" = "firefox.desktop";
-    "x-scheme-handler/http" = "firefox.desktop";
-    "x-scheme-handler/https" = "firefox.desktop";
+    "text/html" = "org.qutebrowser.qutebrowser.desktop";
+    "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
+    "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
   };
 }
