0071d81550535291403053c871929c5685d44fce
dvd <dvd@example.com>
2026-06-28 17:41:28 +0300

    swapped btm for btop 2

--- modules/sysmons/btop/default.nix
+++ modules/sysmons/btop/default.nix
@@ -12,11 +12,12 @@ in
   options.custom.sysmons.btop.enable = mkEnableOption "Enable btop sysmon";
   config = mkIf cfg.enable {
     environment.systemPackages = [ pkgs.btop-cuda ];
-  };
+
   custom.hjem.cfg.xdg.config.files."btop/btop.conf" = {
     source = ./btop.conf;
   };
   custom.hjem.cfg.xdg.config.files."btop/themes/gruvbox_material_dark.theme"= {
     source = ./gruvbox_material_dark.theme;
+  };
   };
 }
