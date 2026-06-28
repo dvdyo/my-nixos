d7badb82a825ce78f9f5c42ba694912f7b2d57b6
dvd <dvd@example.com>
2026-06-28 23:54:16 +0300

    added ovveride to chafa to support webp

--- modules/shell/chafa.nix
+++ modules/shell/chafa.nix
@@ -7,11 +7,15 @@
 let
   cfg = config.custom.shell.chafa;
   inherit (lib) mkEnableOption mkIf;
+  chafa-webp = pkgs.chafa.overrideAttrs (old: {
+  buildInputs = old.buildInputs ++ [ pkgs.libwebp ];
+  });
 in
 {
   options.custom.shell.chafa.enable = mkEnableOption "Enable chafa";
 
   config = mkIf cfg.enable {
-    environment.systemPackages = [ pkgs.chafa ];
+    environment.systemPackages = [ chafa-webp ];
+    
   };
 }
